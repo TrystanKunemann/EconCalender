b1a0601997b1c4a467d216f3eac11fd4040b268d
TrystanKunemann <trystan.kunemann@gmail.com>
2025-05-14 15:26:54 +0100

    Update streamlit_calendar_app.py

--- econcalendar_complete_package_final/streamlit_calendar_app.py
+++ econcalendar_complete_package_final/streamlit_calendar_app.py
@@ -1,3 +1,62 @@
+
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+
+def render_economic_calendar(file_path):
+    calendar_df = pd.read_csv(file_path)
+    calendar_df['Date'] = pd.to_datetime(calendar_df['Date'], dayfirst=True)
+
+    key_events = ['Monetary Policy', 'CPI', 'NFP']
+    unique_currencies = calendar_df['Currency'].unique()
+    color_palette = px.colors.qualitative.Dark24
+    color_map = {currency: color_palette[i % len(color_palette)] for i, currency in enumerate(unique_currencies)}
+
+    holidays_df = calendar_df[~calendar_df['Event'].isin(key_events)]
+    key_events_df = calendar_df[calendar_df['Event'].isin(key_events)]
+
+    fig = go.Figure()
+
+    for currency in holidays_df['Currency'].unique():
+        currency_holidays = holidays_df[holidays_df['Currency'] == currency]
+        rgb = px.colors.hex_to_rgb(color_map[currency])
+        rgba_holiday = f'rgba({rgb[0]}, {rgb[1]}, {rgb[2]}, 0.2)'
+
+        for date in currency_holidays['Date'].unique():
+            fig.add_vrect(
+                x0=date, x1=date,
+                fillcolor=rgba_holiday,
+                line_width=0,
+                annotation_text=currency,
+                annotation_position="top left",
+                layer="below"
+            )
+
+    for currency in key_events_df['Currency'].unique():
+        currency_events = key_events_df[key_events_df['Currency'] == currency]
+        rgb = px.colors.hex_to_rgb(color_map[currency])
+        rgba_event = f'rgba({rgb[0]}, {rgb[1]}, {rgb[2]}, 1.0)'
+
+        for date in currency_events['Date'].unique():
+            fig.add_vrect(
+                x0=date, x1=date,
+                fillcolor=rgba_event,
+                line_width=0,
+                annotation_text=currency,
+                annotation_position="top left",
+                layer="below"
+            )
+
+    fig.update_layout(
+        title="Economic Calendar: Differentiated by Currency & Opacity",
+        xaxis_title="Date",
+        yaxis_title="",
+        yaxis=dict(showticklabels=False),
+        height=700,
+    )
+
+    return fig
+
 import os
 import pandas as pd
 import streamlit as st
